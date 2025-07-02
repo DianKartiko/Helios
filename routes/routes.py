2a6023d2b9d60480aee7c3544bada5259ac29f66
DianKartiko <auliakartiko28@gmail.com>
2025-07-02 21:39:12 +0700

    Design and structuring

--- routes/routes.py
+++ routes/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, Blueprint
+from models.model import social_media
 
 routes = Blueprint('main', __name__)
 
 @routes.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', social_media=social_media)
